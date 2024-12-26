fd7e3fcb9552ae4824b96014600901a59601d2bd
Umberto Domenico Ciccia <108148943+umbertocicciaa@users.noreply.github.com>
2024-12-26 22:49:44 +0100

    updated visitor counter

--- visitor-counter.js
+++ visitor-counter.js
@@ -10,8 +10,9 @@ async function updateVisitorCounter() {
 async function getVisitorCounter() {
     const response = await fetch('https://cloud-resume-gateway-8yv80jb4.ew.gateway.dev/read');
     if (response.ok) {
-        const data = await response.json();
-        document.getElementById('visitor-counter').innerText = data.count;
+        const visitors = await response.json();
+        const visitorCount = visitors[0].counter;
+        document.getElementById('visitor-counter').innerText = visitorCount;
     } else {
         console.error('Errore durante l\'ottenimento del contatore dei visitatori');
     }
